3dec20fa46f65e751be67ce2da5839da7dfdf630
Waqas Khan <71628657+Waqaskhan404@users.noreply.github.com>
2023-03-06 23:05:59 -0500

    Routes Created

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app=express()
 
 const port=process.env.PORT || 5000
 
-app.get("/",function(req,res){
-    res.status(200).json({message:"Hey This is Home Route"})
-})
+
+// Middleware
+app.use("/",require("./routes/contactRoutes"))
 
 app.listen(port,function(req,res){
     console.log(`Server is Running Port  ${port}`)
