612cea16267e7ecffa9dcbf860102e87ccecf3c3
Waqas-CST <waqas.khan.40004@gmail.com>
2023-03-07 12:46:17 +0500

    Read/Get , Create/Post , Update/put and Delete/delete CRUD operation are done here

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,60 +1,83 @@
-const asyncHandler=require("express-async-handler")
-const Contact=require("../models/contactModel")
+const asyncHandler = require("express-async-handler");
+const { deleteOne } = require("../models/contactModel");
+const Contact = require("../models/contactModel");
 
 //  @ desc Get All Contacts
 //  @route GET /api/contacts
 //  @access Public
 
-const getContacts=asyncHandler( async (req,res)=>{
-    //here is the model with await it get all the contact with find() function
-    const contact=await Contact.find();
-    res.status(200).json(contact)
-})
+const getContacts = asyncHandler(async (req, res) => {
+  const contact = await Contact.find();
+  res.status(200).json(contact);
+});
 
 //  @ desc POST  Contact
 //  @route POST /api/contacts
 //  @access Public
-const createContact=asyncHandler( async (req,res)=>{
-    console.log(req.body)
-    // Destructure the Req body
-    const {name,email,phone}=req.body
-    if(!name || !email || !phone){
-        res.status(400);
-        throw new Error("All fields are modulatory")
-    }
-    //now Create a realtime Contact which have a body 
-    const contact=await Contact.create({
-        name,
-        email,
-        phone
-    })
-        res.status(201).json({message:"Create Contacts"})
-    })
+const createContact = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  // Destructure the Req body
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are modulatory");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
 
-    //  @ desc GET  Contact
+//  @ desc GET  Contact
 //  @route GET /api/contacts/:id
 //  @access Public
-const getContact=asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`Get Contact for ${req.params.id}`})
-
-})
-
+const getContact = asyncHandler(async (req, res) => {
+    const contact=await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found")
+    }
+  res.status(200).json(contact);
+});
 
 //  @ desc Update  Contact
 //  @route PUT /api/contacts
 //  @access Public
-const updateContact=asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`Delete Contact for ${req.params.id}`})
-
-})
-
+const updateContact = asyncHandler(async(req, res) => {
+    const contact=await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact Not Found")
+    }
+    const updateContact=await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new:true
+        }
+    )
+  res.status(200).json(updateContact);
+});
 
 //  @ desc DELETE  Contact
 //  @route DELETE /api/contacts
 //  @access Public
-const deleteContact=asyncHandler( async (req,res)=>{
-    res.status(200).json({message:`Delete Contact for ${req.params.id}`})
-})
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(contact);
+  });
 
-
-module.exports={getContacts,createContact,getContact,updateContact,deleteContact}
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,51 +1,43 @@
-const {constrants}=require("../constrants")
-const errorHandler=(err,req,res,next)=>{
+const { constant } = require("../constant");
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case 400:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case 401:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case 403:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case 404:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
 
-    const statusCode=res.statusCode ? res.statusCode : 500
-    // res.json({message:"Not Found" , message:err.message,stackTrace:err.stack})
-    switch (statusCode) {
-        case constrants.VALIDATION_ERROR:
-            res.json({
-            title:"Validation Faild",
-            message:err.message,
-            stackTrace:err.stack
-            })
-            break;
-            case constrants.UNAUTHORIZED:
-            res.json({
-            title:"unAuthorized",
-            message:err.message,
-            stackTrace:err.stack
-            })
-            break;
-            case constrants.FORBIDDEN:
-            res.json({
-            title:"Forbidden",
-            message:err.message,
-            stackTrace:err.stack
-            })
-            break;
-            case constrants.SERVER_ERROR:
-            res.json({
-            title:"Server Error",
-            message:err.message,
-            stackTrace:err.stack
-            })
-            break;
-            case constrants.NOT_FOUND:
-            res.json({
-            title:"Not Found Error",
-            message:err.message,
-            stackTrace:err.stack
-            })
-            break;
-    
-        default:
-            console.log("No Error  All are Good")
-            break;
-    }
+    case 500:
+      res.json({
+        title: "Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    default:
+      console.log("No Error, All good !");
+      break;
+  }
+};
 
-
-}
-
-module.exports=errorHandler
+module.exports = errorHandler;
